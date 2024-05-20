f1cd73caf557eb7d1dafab92d05db298991bc4c3
OikawaToru1 <aniketadhikari2@gmail.com>
2024-05-20 19:33:04 +0545

    Added functionality to add sub and multiply

---
+++ script.js
@@ -0,0 +1,86 @@
+const calculation = document.getElementById("calculation");
+const answer = document.getElementById("ans");
+const operator = document.getElementById("operator");
+const num = document.getElementById("num2");
+
+
+let num1=0, num2 =0;
+let count =1;
+
+
+const buttons = document.querySelectorAll("button");
+console.log(buttons.length);
+buttons.forEach((btn)=>{
+    btn.addEventListener("click",()=>{
+       if(btn.id=="number" && count >=1)
+        {   
+            calculation.innerHTML = calculation.innerHTML + btn.value;
+            calculation.value = btn.value;
+        }
+        else if(btn.id == "operator"){
+          operator.value = btn.value;
+          operator.innerHTML= btn.value;
+          count =0;
+        }
+        else if(btn.id == "number")
+        {
+            num.value = btn.value;
+            num.innerHTML = btn.value;
+
+        }
+        else
+        {
+            calc();
+            count =1;
+        }
+    })
+});
+
+function calc()
+{
+   let num1 = parseInt(calculation.innerText);
+   let num2 = parseInt(num.innerText);
+   let opt = operator.innerText;
+  if(opt == "+")
+    {
+        sum(num1,num2);
+    }
+    else if(opt == "-")
+        {
+            sub(num1,num2);
+        }
+    else if(opt == "*")
+        {
+            mult(num1,num2);
+        }
+}
+
+function sum(n1,n2)
+{   
+    let sm = n1+n2;
+    answer.innerHTML= sm;
+    return sm;
+    
+}
+function sub(n1,n2)
+{   
+    let sm = n1-n2;
+    answer.innerHTML= sm;
+    return sm;
+    
+}
+function mult(n1,n2)
+{   
+    let sm = n1*n2;
+    answer.innerHTML= sm;
+    return sm;   
+}
+
+function div(n1,n2)
+{
+    let dv = n1/n2;
+    answer.innerHTML = dv;
+    return dv;
+}
+
+

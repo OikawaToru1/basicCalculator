7cb8b2338ecb60675a14a0e3077898087f2ee4ac
OikawaToru1 <aniketadhikari2@gmail.com>
2024-05-23 11:46:03 +0545

    Added feature to do multiple oprerations

--- script.js
+++ script.js
@@ -6,33 +6,49 @@ const clear = document.getElementById("clear");
 const remove = document.getElementById("remove");
 let num1=0, num2 =0;
 let count =1;
+let calculationDOne = false;
 
 
 const buttons = document.querySelectorAll("button");
 console.log(buttons.length);
 buttons.forEach((btn)=>{
     btn.addEventListener("click",()=>{
-       if(btn.id=="number" && count >=1)
+       if(btn.id=="number" && count >=1 && !calculationDOne)
         {   
             calculation.innerHTML = calculation.innerHTML + btn.value;
             calculation.value = btn.value;
         }
-        else if(btn.id == "operator"){
+        else if(btn.id == "operator" && !calculationDOne){
           operator.value = btn.value;
           operator.innerHTML= btn.value;
           count =0;
         }
-        else if(btn.id == "number" && count ==0)
+        else if(btn.id == "number" && count ==0 && !calculationDOne)
         {
             num.value = btn.value;
             num.innerHTML = num.innerHTML + btn.value;
 
         }
-        else
+        else if(btn.id=="operate")
         {
             calc();
             count =1;
         }
+        else if (btn.id =="operator" && calculationDOne)
+            {
+                operator.innerHTML=btn.value;
+                num.innerHTML="";
+                calculation.innerHTML= answer.innerHTML;
+                answer.innerHTML= 0;
+                alert(`Calculation is done: ${calculationDOne}`);
+                count =0;
+
+            }
+        else if(btn.id =="number" && calculationDOne)
+            {
+                num.value = btn.value;
+                 num.innerHTML = num.innerHTML + btn.value;
+            }
     })
 });
 
@@ -57,12 +73,17 @@ function calc()
         {
             div(num1,num2);
         }
+    else if (opt == "%")
+    {
+
+    }
 }
 
 function sum(n1,n2)
 {   
     let sm = n1+n2;
     answer.innerHTML= sm;
+    calculationDOne = true;
     return sm;
     
 }
@@ -70,6 +91,7 @@ function sub(n1,n2)
 {   
     let sm = n1-n2;
     answer.innerHTML= sm;
+    calculationDOne = true;
     return sm;
     
 }
@@ -77,6 +99,7 @@ function mult(n1,n2)
 {   
     let sm = n1*n2;
     answer.innerHTML= sm;
+    calculationDOne = true;
     return sm;   
 }
 
@@ -84,6 +107,14 @@ function div(n1,n2)
 {
     let dv = n1/n2;
     answer.innerHTML = dv;
+    calculationDOne = true;
+    return dv;
+}
+function percent(n1)
+{
+    let dv = n1/n2;
+    answer.innerHTML = dv;
+    calculationDOne = true;
     return dv;
 }
 
@@ -106,6 +137,7 @@ remove.addEventListener("click",()=>
                     else if (operator.innerHTML != "")
                         {
                             operator.innerHTML="";
+                            answer.innerHTML="0";
                         }
                     else
                     {

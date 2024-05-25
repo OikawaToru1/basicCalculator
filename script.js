8625ee96b3d0fd93fb256ac924f18a9937134dc5
OikawaToru1 <aniketadhikari2@gmail.com>
2024-05-25 13:51:37 +0545

    Added remanining functions inverse,squareroot,...

--- script.js
+++ script.js
@@ -3,10 +3,15 @@ const answer = document.getElementById("ans");
 const operator = document.getElementById("operator");
 const num = document.getElementById("num2");
 const clear = document.getElementById("clear");
+const clearAll = document.getElementById("clearScreen");
 const remove = document.getElementById("remove");
+const square = document.getElementById("square");
+const sqroot = document.getElementById("root");
+const inverse = document.getElementById("inverse");
 let num1=0, num2 =0;
 let count =1;
 let test1=0,test2=0;
+let operatorPressed = false;
 let calculationDOne = false;
 
 
@@ -17,13 +22,14 @@ buttons.forEach((btn)=>{
        if(btn.id=="number" && count >=1 && !calculationDOne)
         {   
             calculation.innerHTML = calculation.innerHTML + btn.value;
-            calculation.value = btn.value;
+            calculation.value = calculation.innerText;
             test1 = calculation.value;
         }
-        else if(btn.id == "operator" && !calculationDOne){
+        else if(btn.id == "operator" && !calculationDOne && !operatorPressed){
           operator.value = btn.value;
           operator.innerHTML= btn.value;
           count =0;
+          operatorPressed= true;
         }
         else if(btn.id == "number" && count ==0 && !calculationDOne)
         {
@@ -37,13 +43,12 @@ buttons.forEach((btn)=>{
             calc();
             count =1;
         }
-        else if (btn.id =="operator" && calculationDOne)
+        else if (btn.id =="operator" && calculationDOne && operatorPressed)
             {
+                calc();
                 operator.innerHTML=btn.value;
-                num.innerHTML="";
                 calculation.innerHTML= answer.innerHTML;
-                answer.innerHTML= 0;
-                alert(`Calculation is done: ${calculationDOne}`);
+                num.innerHTML="";
                 count =0;
 
             }
@@ -57,8 +62,8 @@ buttons.forEach((btn)=>{
 
 function calc()
 {
-   let num1 = parseInt(calculation.innerText);
-   let num2 = parseInt(num.innerText);
+   let num1 = parseFloat(calculation.innerText);
+   let num2 = parseFloat(num.innerText);
    let opt = operator.innerText;
   if(opt == "+")
     {
@@ -78,17 +83,16 @@ function calc()
         }
     else if (opt == "%")
         {
-            if(test1!=0 && test2==0)
+            if(test2==0)
                 {
-                    percent(test1);
+                    percent(calculation.innerText);
                 }
-            else if(test1==0 && ntest!=0)
+            else if(test1==0)
                 {
-                    percent(test2);
+                    percent(num.innerText);
                 }
-            else 
+            else if(test1!=0 && test2!=0)
                 {
-                    calc();
                     percent(answer.value)
                 }
         }
@@ -99,7 +103,8 @@ function sum(n1,n2)
     let sm = n1+n2;
     answer.innerHTML= sm;
     calculationDOne = true;
-    return sm;
+    test2=0;
+    return sm , test2;
     
 }
 function sub(n1,n2)
@@ -107,7 +112,7 @@ function sub(n1,n2)
     let sm = n1-n2;
     answer.innerHTML= sm;
     calculationDOne = true;
-    return sm;
+    
     
 }
 function mult(n1,n2)
@@ -115,15 +120,17 @@ function mult(n1,n2)
     let sm = n1*n2;
     answer.innerHTML= sm;
     calculationDOne = true;
-    return sm;   
+    test2=0;
+    return sm,test2;   
 }
 
 function div(n1,n2)
 {
     let dv = n1/n2;
-    answer.innerHTML = dv;
+    answer.innerHTML = dv.toFixed(3);
     calculationDOne = true;
-    return dv;
+    test2 =0;
+    return dv, test2;
 }
 function percent(num)
 {
@@ -140,6 +147,18 @@ clear.addEventListener("click",()=>{
     operator.innerHTML="";
     num.innerHTML="";
     calculationDOne = false;
+    operatorPressed= false;
+    count=1;
+
+});
+clearAll.addEventListener("click",()=>{
+    calculation.innerHTML="";
+    answer.innerHTML= "0";
+    operator.innerHTML="";
+    num.innerHTML="";
+    calculationDOne = false;
+    operatorPressed= false;
+    count=1;
 
 });
 
@@ -153,7 +172,7 @@ remove.addEventListener("click",()=>
                     else if (operator.innerHTML != "")
                         {
                             operator.innerHTML="";
-                            answer.innerHTML="0";
+                            operatorPressed = false;
                         }
                     else
                     {
@@ -161,4 +180,47 @@ remove.addEventListener("click",()=>
                         count =1;
                     }
                 }
-)
+);
+
+square.addEventListener("click",()=>{
+    if(test2==0)
+    {
+        let sq = calculation.innerText*calculation.innerText;
+        answer.innerHTML = sq.toFixed(3);
+    }
+    else
+    {
+        calc();
+        sq = answer.innerText*answer.innerText;
+        answer.innerHTML = sq.toFixed(3);
+    }
+});
+
+sqroot.addEventListener("click",()=>{
+    if(test2==0)
+        {
+            let sq = Math.sqrt(calculation.innerText);
+            answer.innerHTML = sq.toFixed(3);
+        }
+        else
+        {
+            calc();
+            sq = Math.sqrt(answer.innerText);
+            answer.innerHTML = sq.toFixed(3);
+        }
+    
+});
+
+inverse.addEventListener("click",()=>{
+    if(test2==0)
+        {
+            let sq = 1/(calculation.innerText);
+            answer.innerHTML = sq.toFixed(3);
+        }
+        else
+        {
+            calc();
+            sq = 1/(answer.innerText);
+            answer.innerHTML = sq.toFixed(3);
+        }
+})
